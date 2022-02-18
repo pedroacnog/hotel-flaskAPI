cbadf52d1e8dcc58da368caed42d876c190a6b60
pedro <pedroacnog@gmail.com>
2022-02-18 13:30:27 -0300

    add: resource folder

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask_restful import Api, Resource
+from flask_restful import Api
 
 from flask import Flask
+from resources.hotel import Hoteis
 
 app = Flask(__name__)
 api = Api(app)
 
-class Hoteis(Resource):
-  def get(self):
-    return {'hoteis': 'meus hoteis'}
-
-
 api.add_resource(Hoteis, '/hoteis')
-
 
 if __name__ == '__main__':
   app.run(debug=True)
