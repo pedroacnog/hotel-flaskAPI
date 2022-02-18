5d8e44dd984fcbc11ac1a24f8022d77fe6f171c5
pedro <pedroacnog@gmail.com>
2022-02-18 13:41:41 -0300

    feat: get hoteis

--- resources/hotel.py
+++ resources/hotel.py
@@ -24,6 +24,23 @@ hoteis = [
   }
 ]
 
-class Hoteis(Resource):
+class Hoteis(Resource): # Referente ao GET de todos os Hoteis
   def get(self):
     return {'hoteis': hoteis}
+
+class Hotel(Resource):
+  
+  def get(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
+    for hotel in hoteis:
+      if hotel['hotel_id'] == hotel_id:
+        return hotel
+    return {'message': 'Hotel not found.'}, 404
+
+  def post(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
+    pass
+
+  def put(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
+    pass
+
+  def delete(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
+    pass
