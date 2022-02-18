d95031e46fd204fc161908f8588fa9c4330c7cc9
pedro <pedroacnog@gmail.com>
2022-02-18 14:21:27 -0300

    feat: put hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -28,36 +28,46 @@ class Hoteis(Resource): # Referente ao GET de todos os Hoteis
   def get(self):
     return {'hoteis': hoteis}
 
+
 class Hotel(Resource):
-  
-  def get(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
+
+  argumentos = reqparse.RequestParser()
+  argumentos.add_argument('nome')
+  argumentos.add_argument('estrelas')
+  argumentos.add_argument('diaria')
+  argumentos.add_argument('cidade')
+
+  def find_hotel(hotel_id):
     for hotel in hoteis:
       if hotel['hotel_id'] == hotel_id:
         return hotel
+    return None
+
+  def get(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
+    hotel = Hotel.find_hotel(hotel_id)
+    if hotel:
+      return hotel
     return {'message': 'Hotel not found.'}, 404
 
   def post(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
-    argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
-    argumentos.add_argument('diaria')
-    argumentos.add_argument('cidade')
+    dados = Hotel.argumentos.parse_args()
 
-    dados = argumentos.parse_args()
-
-    novo_hotel = {
-      'hotel_id': hotel_id,
-      'nome': dados['nome'],
-      'estrelas': dados['estrelas'],
-      'diaria': dados['diaria'],
-      'cidade': dados['cidade'],
-    }
+    novo_hotel = { 'hotel_id': hotel_id, **dados }
 
     hoteis.append(novo_hotel)
     return novo_hotel, 200
 
   def put(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
-    pass
+    dados = Hotel.argumentos.parse_args()
+    novo_hotel = { 'hotel_id': hotel_id, **dados }
+
+    hotel = Hotel.find_hotel(hotel_id)
+
+    if hotel:
+      hotel.update(novo_hotel)
+      return novo_hotel, 200
+    hoteis.append(novo_hotel)
+    return novo_hotel, 201
 
   def delete(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
     pass
