e966f573ee5853e7509a54ed86151f250ae1a3f0
pedro <pedroacnog@gmail.com>
2022-02-18 13:15:47 -0300

    create project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask_restful import Api, Resource
+
+from flask import Flask
+
+app = Flask(__name__)
+api = Api(app)
+
+class Hoteis(Resource):
+  def get(self):
+    return {'hoteis': 'meus hoteis'}
+
+
+api.add_resource(Hoteis, '/hoteis')
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
