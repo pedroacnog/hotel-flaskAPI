c94d61eb2aa0dc9f9705c73402ca738d53a7f854
pedro <pedroacnog@gmail.com>
2022-02-21 13:25:18 -0300

    improve: hotel model

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
   {
@@ -52,14 +53,17 @@ class Hotel(Resource):
   def post(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
     dados = Hotel.argumentos.parse_args()
 
-    novo_hotel = { 'hotel_id': hotel_id, **dados }
+    hotel_objeto = HotelModel(hotel_id, **dados) #construtor da classe HotelModel onde é criado um objeto do Hotel
+    novo_hotel = hotel_objeto.json() # 
 
     hoteis.append(novo_hotel)
     return novo_hotel, 200
 
   def put(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
     dados = Hotel.argumentos.parse_args()
-    novo_hotel = { 'hotel_id': hotel_id, **dados }
+
+    hotel_objeto = HotelModel(hotel_id, **dados)
+    novo_hotel = hotel_objeto.json()
 
     hotel = Hotel.find_hotel(hotel_id)
 
