3de027af2689441ff30954e1f3fc7476b0bafb70
pedro <pedroacnog@gmail.com>
2022-02-25 14:00:16 -0300

    refactor: post hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,29 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-  {
-    'hotel_id': 'alpha',
-    'nome': 'Alpha Hotel',
-    'estrelas': 4.3,
-    'diaria': 420.34,
-    'cidade': 'Rio de Janeiro'
-  },
-  {
-    'hotel_id': 'bravo',
-    'nome': 'Bravo Hotel',
-    'estrelas': 4.4,
-    'diaria': 380.90,
-    'cidade': 'Pelotas'
-  },
-  {
-    'hotel_id': 'charlie',
-    'nome': 'Charlie Hotel',
-    'estrelas': 3.9,
-    'diaria': 320.24,
-    'cidade': 'Balneario Camboriu'
-  }
-]
 
 class Hoteis(Resource): # Referente ao GET de todos os Hoteis
   def get(self):
@@ -31,18 +8,11 @@ class Hoteis(Resource): # Referente ao GET de todos os Hoteis
 
 
 class Hotel(Resource):
-
   argumentos = reqparse.RequestParser()
   argumentos.add_argument('nome')
   argumentos.add_argument('estrelas')
   argumentos.add_argument('diaria')
   argumentos.add_argument('cidade')
-
-  def find_hotel(hotel_id):
-    for hotel in hoteis:
-      if hotel['hotel_id'] == hotel_id:
-        return hotel
-    return None
 
   def get(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
     hotel = Hotel.find_hotel(hotel_id)
@@ -51,12 +21,14 @@ class Hotel(Resource):
     return {'message': 'Hotel not found.'}, 404
 
   def post(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
+    if HotelModel.find_hotel(hotel_id):
+      return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400
+    
     dados = Hotel.argumentos.parse_args()
+    hotel = HotelModel(hotel_id, **dados)
+    hotel.save_hotel()
+    return hotel.json()
 
-    hotel_objeto = HotelModel(hotel_id, **dados) #construtor da classe HotelModel onde é criado um objeto do Hotel
-    novo_hotel = hotel_objeto.json() # 
-
-    hoteis.append(novo_hotel)
     return novo_hotel, 200
 
   def put(self, hotel_id): # Recebe o hotel_id da url /hoteis/hotel_id
