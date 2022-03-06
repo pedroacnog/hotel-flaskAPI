f4f46ca0107fcd91dcda0df194066a0ffc1a7bdb
Joao Pedro <pedroacnog@gmail.com>
2022-03-06 18:01:30 -0300

    add: jwt aut

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,5 +1,11 @@
 from models.usuario import UserModel
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
+
+argumentos = reqparse.RequestParser()
+argumentos.add_argument('login', type=str, required=True, help="The field 'login' cammpt be left blank")
+argumentos.add_argument('senha', type=str, required=True, help="The field 'senha' cammpt be left blank")
 
 class User(Resource):
   # /usuarios/{user_id}
@@ -20,12 +26,10 @@ class User(Resource):
     return {'message': 'User not found.'}, 404
      
 class UserRegister(Resource):
+
   # /cadastro
 
   def post(self):
-    argumentos = reqparse.RequestParser()
-    argumentos.add_argument('login', type=str, required=True, help="The field 'login' cammpt be left blank")
-    argumentos.add_argument('senha', type=str, required=True, help="The field 'senha' cammpt be left blank")
     dados = argumentos.parse_args()
 
     if UserModel.find_by_login(dados['login']):
@@ -34,3 +38,15 @@ class UserRegister(Resource):
     user = UserModel(**dados)
     user.save_user()
     return {"message": "User created successfully."}, 201
+
+class UserLogin(Resource):
+
+  @classmethod
+  def post(cls):
+    dados = argumentos.parse_args()
+
+    user = UserModel.find_by_login(dados['login'])
+    if user and safe_str_cmp(user.senha, dados['senha']):
+      token = create_access_token(identity=user.user_id)
+      return {"acess_token": token}, 200
+    return {"message": "The username or password is incorrect."}, 401
